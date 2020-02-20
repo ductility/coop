76de622c3045e8c08b4af3b0b6f668614ab340ed
ductility <jys9704@gmail.com>
2020-02-20 11:40:54 +0900

    2020/02/20 add remark

--- include/modified_teleop/modified_teleop.h
+++ include/modified_teleop/modified_teleop.h
@@ -1,5 +1,6 @@
 #ifndef OPEN_MANIPULATOR_TELEOP_H
 #define OPEN_MANIPULATOR_TELEOP_H
+//OPEN_MANIPULATOR_TELEOP에 OPEN_MANIPULATOR_CONTROL_GUI, OPEN_MANIPULATOR_MASTER_SLAVE 를 참고하여 만들었음
 
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
@@ -7,9 +8,7 @@
 #include <termios.h>
 #include "open_manipulator_msgs/SetJointPosition.h"
 #include "open_manipulator_msgs/SetKinematicsPose.h"
-
-//Actuator Enable/Disable
-#include "open_manipulator_msgs/SetActuatorState.h"
+#include "open_manipulator_msgs/SetActuatorState.h" //Actuator 토크 제어를 위해 추가
 
 #include <geometry_msgs/PoseStamped.h>
 
@@ -19,12 +18,11 @@
 #include <std_msgs/Bool.h>
 
 #define NUM_OF_JOINT 4
-#define DELTA 0.01
-#define JOINT_DELTA 0.05
+// #define DELTA 0.01
+// #define JOINT_DELTA 0.05
 #define PATH_TIME 2
 #define LOOP_RATE 0.1
 
-//Buffer
 typedef struct _WaypointBuffer
 {
   std::vector<double> joint_angle;
@@ -38,43 +36,35 @@ class OpenManipulatorTeleop
   ros::NodeHandle node_handle_;
   ros::NodeHandle priv_node_handle_;
 
-  ros::ServiceClient goal_joint_space_path_from_present_client_;
-  ros::ServiceClient goal_task_space_path_from_present_position_only_client_;
+  //Client
   ros::ServiceClient goal_joint_space_path_client_;
-  ros::ServiceClient goal_tool_control_client_;
-  ros::ServiceClient goal_task_space_path_position_only_client_;
-  //position + orientation
   ros::ServiceClient goal_task_space_path_client_;
-  //set_actuator_state_client_;
+  ros::ServiceClient goal_task_space_path_position_only_client_;
+  ros::ServiceClient goal_tool_control_client_;
   ros::ServiceClient set_actuator_state_client_;
 
+  //Subscriber 
   ros::Subscriber joint_states_sub_;
   ros::Subscriber kinematics_pose_sub_;
-  ros::Subscriber input_kinematics_pose_sub_;//kk
-  ros::Subscriber input_gripper_sub_;//gripper(input)
-  ros::Subscriber input_actuator_sub_;//Actuator(input)
-  ros::Subscriber position_stamp_sub_;//position stamp
-  ros::Subscriber hand_guide_move_point_sub_;//hand guide move point
-  ros::Subscriber hand_guide_move_path_sub_;//hand guide move path
+  ros::Subscriber input_kinematics_pose_sub_;
+  ros::Subscriber input_gripper_sub_;
+  ros::Subscriber input_actuator_sub_;
+  ros::Subscriber position_stamp_sub_;
+  ros::Subscriber hand_guide_move_point_sub_;
+  ros::Subscriber hand_guide_move_path_sub_;
 
+  //전역변수 선언
+  std::vector<std::string> joint_name_;
   std::vector<double> present_joint_angle_;
-  std::vector<double> present_gripper_angle_;//추가
+  std::vector<double> present_gripper_angle_;
   std::vector<double> present_kinematic_position_;
-  std::vector<double> input_kinematic_position_;//goal position(input)
-  std::vector<double> input_kinematic_orientation_;//goal orientation (input)
-  std::vector<double> input_gripper_angle_;//gripper angle(input)
-  std::vector<double> start_joint_stamp_;//position stamp
-  std::vector<double> end_joint_stamp_;//position stamp
-  open_manipulator_msgs::KinematicsPose kinematics_pose_;
-
-  double service_call_period_;//hand huide record
-
+  std::vector<double> input_kinematic_position_;
+  std::vector<double> input_kinematic_orientation_;
+  std::vector<double> input_gripper_angle_;
+  std::vector<double> start_joint_stamp_;
+  std::vector<double> end_joint_stamp_;
   std::vector<WaypointBuffer> record_buffer_;
   int buffer_index_ = 0;
-
-
-
-  struct termios oldt_;
 
  public:
 
@@ -84,47 +74,38 @@ class OpenManipulatorTeleop
   void initClient();
   void initSubscriber();
 
+  //Callback함수 선언, node-red 컨트롤러에서 발행된 토픽을 받으면 실행
   void jointStatesCallback(const sensor_msgs::JointState::ConstPtr &msg);
   void kinematicsPoseCallback(const open_manipulator_msgs::KinematicsPose::ConstPtr &msg);
+  void kinematicsPoseInputCallback(const geometry_msgs::Pose::ConstPtr &msg);
+  void gripperInputCallback(const std_msgs::Float64::ConstPtr &msg);
+  void ActuatorStateCallback(const std_msgs::Bool::ConstPtr &msg);
+  void positionStampCallback(const std_msgs::Bool::ConstPtr &msg);
+  void handGuideMovePointCallback(const std_msgs::Bool::ConstPtr &msg);
+  void handGuideMovePathCallback(const std_msgs::Bool::ConstPtr &msg);
 
-  double getServiceCallPeriod() {return service_call_period_;}//hand guide record
-
-  void kinematicsPoseInput(const geometry_msgs::Pose::ConstPtr &msg);//input
-  void gripperInput(const std_msgs::Float64::ConstPtr &msg);//input
-  void ActuatorStateInput(const std_msgs::Bool::ConstPtr &msg);//Actuator input
-  void positionStamp(const std_msgs::Bool::ConstPtr &msg);//Position Stamp
-  void handGuideMovePoint(const std_msgs::Bool::ConstPtr &msg);//hand guide move point
-  void handGuideMovePath(const std_msgs::Bool::ConstPtr &msg);//hand guide move path
-
-
+  //Manipulator의 실시간 정보 가져오기
   std::vector<double> getPresentJointAngle();
   std::vector<double> getPresentGripperAngle();
   std::vector<double> getPresentKinematicsPose();
-  std::vector<double> getInputKinematicsPose();//input
-  
-  bool getOpenManipulatorActuatorState();//Actuator
-  bool setActuatorState(bool actuator_state);//setActuator
 
-  bool stamp = false;//레코드 할 때 참 -> 루프돌리기 거짓 루프탈출.
-  bool playstamp = false;// 레코드 실행하기
+  //Actuator ON/OFF
+  bool setActuatorState(bool actuator_state);
 
-  bool setJointSpacePathFromPresent(std::vector<std::string> joint_name, std::vector<double> joint_angle, double path_time);
+  //handguide 레코딩과 실행을 위한 bool 선언
+  bool stamp = false;// true : 레코드 루프돌리기, false : 루프탈출.
+  bool playstamp = false;// true : 레코드 실행하기
+
+  //Manipulator를 특정 각도 또는 위치로 이동시키기 위한 함수 선언. 함수 내에 service call 하는 내용 포함.
   bool setJointSpacePath(std::vector<std::string> joint_name, std::vector<double> joint_angle, double path_time);
-  bool setTaskSpacePathFromPresentPositionOnly(std::vector<double> kinematics_pose, double path_time);
   bool setTaskSpacePathPositionOnly(std::vector<double> kinematics_pose, double path_time);
-  //added
   bool setTaskSpacePath(std::vector<double> kinematics_pose, std::vector<double> kinematics_orientation, double path_time);
-
   bool setToolControl(std::vector<double> joint_angle);
 
-  void publishCallback(const ros::TimerEvent&); // hand guide record
-
-  void printText();
-  void setGoal(char ch);
-
-  void restoreTerminalSettings(void);
-  void disableWaitingForEnter(void);
-
+  //기타 함수
+  void publishCallback(const ros::TimerEvent&);//특정 시간간격으로 루프를 돌게 함
+  void printBufferSize();
+  void setMode(char ch);//task space path를 이용한 이동에서 position만 고려할 것인지, orientation도 같이 고려할 것인지 선택
 };
 
 #endif //OPEN_MANIPULATOR_TELEOP_H
--- src/modified_teleop.cpp
+++ src/modified_teleop.cpp
@@ -23,6 +23,11 @@ OpenManipulatorTeleop::OpenManipulatorTeleop()
     :node_handle_(""), 
      priv_node_handle_("~")
 {
+  //전역변수 초기화
+  joint_name_.push_back("joint1");
+  joint_name_.push_back("joint2");
+  joint_name_.push_back("joint3");
+  joint_name_.push_back("joint4");
   present_joint_angle_.resize(NUM_OF_JOINT);
   present_gripper_angle_.resize(1);
   present_kinematic_position_.resize(3);
@@ -48,30 +53,22 @@ OpenManipulatorTeleop::~OpenManipulatorTeleop()
 
 void OpenManipulatorTeleop::initClient()
 {
-  goal_joint_space_path_from_present_client_ = node_handle_.serviceClient<open_manipulator_msgs::SetJointPosition>("goal_joint_space_path_from_present");
-  goal_task_space_path_from_present_position_only_client_ = node_handle_.serviceClient<open_manipulator_msgs::SetKinematicsPose>("goal_task_space_path_from_present_position_only");
+  goal_joint_space_path_client_ = node_handle_.serviceClient<open_manipulator_msgs::SetJointPosition>("goal_joint_space_path");
   goal_task_space_path_client_ = node_handle_.serviceClient<open_manipulator_msgs::SetKinematicsPose>("goal_task_space_path");
   goal_task_space_path_position_only_client_ = node_handle_.serviceClient<open_manipulator_msgs::SetKinematicsPose>("goal_task_space_path_position_only");
-
-  goal_joint_space_path_client_ = node_handle_.serviceClient<open_manipulator_msgs::SetJointPosition>("goal_joint_space_path");
   goal_tool_control_client_ = node_handle_.serviceClient<open_manipulator_msgs::SetJointPosition>("goal_tool_control");
-
-  //Actuator_state_client
   set_actuator_state_client_ = node_handle_.serviceClient<open_manipulator_msgs::SetActuatorState>("set_actuator_state");
 }
 void OpenManipulatorTeleop::initSubscriber()
 {
   joint_states_sub_ = node_handle_.subscribe("joint_states", 10, &OpenManipulatorTeleop::jointStatesCallback, this);
   kinematics_pose_sub_ = node_handle_.subscribe("kinematics_pose", 10, &OpenManipulatorTeleop::kinematicsPoseCallback, this);
-
-  input_kinematics_pose_sub_ = node_handle_.subscribe("input_kinematics_pose", 10, &OpenManipulatorTeleop::kinematicsPoseInput, this);
-  input_gripper_sub_ = node_handle_.subscribe("input_gripper_angle", 10, &OpenManipulatorTeleop::gripperInput, this);
-  input_actuator_sub_ = node_handle_.subscribe("input_actuator_state", 10, &OpenManipulatorTeleop::ActuatorStateInput, this);//Actuator controll
-
-  position_stamp_sub_ = node_handle_.subscribe("joint_angle_stamp", 10, &OpenManipulatorTeleop::positionStamp, this);//position stamp start end
-
-  hand_guide_move_point_sub_ = node_handle_.subscribe("hand_guide_move_point", 10, &OpenManipulatorTeleop::handGuideMovePoint, this);//hand guide move point
-  hand_guide_move_path_sub_ = node_handle_.subscribe("hand_guide_move_path", 10, &OpenManipulatorTeleop::handGuideMovePath, this);//hand guide move path
+  input_kinematics_pose_sub_ = node_handle_.subscribe("input_kinematics_pose", 10, &OpenManipulatorTeleop::kinematicsPoseInputCallback, this);
+  input_gripper_sub_ = node_handle_.subscribe("input_gripper_angle", 10, &OpenManipulatorTeleop::gripperInputCallback, this);
+  input_actuator_sub_ = node_handle_.subscribe("input_actuator_state", 10, &OpenManipulatorTeleop::ActuatorStateCallback, this);
+  position_stamp_sub_ = node_handle_.subscribe("joint_angle_stamp", 10, &OpenManipulatorTeleop::positionStampCallback, this);//position stamp 찍기
+  hand_guide_move_point_sub_ = node_handle_.subscribe("hand_guide_move_point", 10, &OpenManipulatorTeleop::handGuideMovePointCallback, this);
+  hand_guide_move_path_sub_ = node_handle_.subscribe("hand_guide_move_path", 10, &OpenManipulatorTeleop::handGuideMovePathCallback, this);
 }
 
 void OpenManipulatorTeleop::jointStatesCallback(const sensor_msgs::JointState::ConstPtr &msg)
@@ -89,7 +86,7 @@ void OpenManipulatorTeleop::jointStatesCallback(const sensor_msgs::JointState::C
   }
   present_joint_angle_ = temp_angle;
 
-  if(!msg->name.at(4).compare("gripper")) temp_gripper_angle.at(0) = (msg->position.at(4));
+  if(!msg->name.at(4).compare("gripper")) temp_gripper_angle.at(0) = (msg->position.at(4));//gripper angle 값을 가져옴
   present_gripper_angle_ = temp_gripper_angle;
 }
 
@@ -102,7 +99,7 @@ void OpenManipulatorTeleop::kinematicsPoseCallback(const open_manipulator_msgs::
   present_kinematic_position_ = temp_position;
 }
 
-void OpenManipulatorTeleop::kinematicsPoseInput(const geometry_msgs::Pose::ConstPtr &msg)
+void OpenManipulatorTeleop::kinematicsPoseInputCallback(const geometry_msgs::Pose::ConstPtr &msg)
 {
   std::vector<double> temp_position;
   std::vector<double> temp_orientation;
@@ -118,22 +115,19 @@ void OpenManipulatorTeleop::kinematicsPoseInput(const geometry_msgs::Pose::Const
 
   input_kinematic_position_ = temp_position;
   input_kinematic_orientation_ = temp_orientation;
-  ROS_INFO("run\n");
-  setGoal('6');
-  // while(buffer_index_ < 99){  setGoal('9');}
-  // buffer_index_ = 0;
+  ROS_INFO("Move by set task space path\n");
+  setMode('1');
 }
 
-void OpenManipulatorTeleop::gripperInput(const std_msgs::Float64::ConstPtr &msg)
+void OpenManipulatorTeleop::gripperInputCallback(const std_msgs::Float64::ConstPtr &msg)
 {
   std::vector<double> joint_angle;
   joint_angle.push_back(msg->data);
   setToolControl(joint_angle);
-  ROS_INFO("gripper on/of \n");
+  ROS_INFO("gripper on/off \n");
 }
 
-//Actuator Enable or Disable
-void OpenManipulatorTeleop::ActuatorStateInput(const std_msgs::Bool::ConstPtr &msg)
+void OpenManipulatorTeleop::ActuatorStateCallback(const std_msgs::Bool::ConstPtr &msg)
 {
   setActuatorState(msg->data);
   if(msg->data){
@@ -144,21 +138,7 @@ void OpenManipulatorTeleop::ActuatorStateInput(const std_msgs::Bool::ConstPtr &m
   }
 }
 
-//Actuator
-bool OpenManipulatorTeleop::setActuatorState(bool actuator_state)
-{
-  open_manipulator_msgs::SetActuatorState srv;
-  srv.request.set_actuator_state = actuator_state;
-
-  if(set_actuator_state_client_.call(srv))
-  {
-    return srv.response.is_planned;
-  }
-  return false;
-}
-
-//positionStamp
-void OpenManipulatorTeleop::positionStamp(const std_msgs::Bool::ConstPtr &msg)
+void OpenManipulatorTeleop::positionStampCallback(const std_msgs::Bool::ConstPtr &msg)
 {
   stamp = msg->data;
   if(stamp){
@@ -171,25 +151,31 @@ void OpenManipulatorTeleop::positionStamp(const std_msgs::Bool::ConstPtr &msg)
   }
 }
 
-//hand guide move point
-void OpenManipulatorTeleop::handGuideMovePoint(const std_msgs::Bool::ConstPtr &msg)
+void OpenManipulatorTeleop::handGuideMovePointCallback(const std_msgs::Bool::ConstPtr &msg)
 {
-  std::vector<std::string> joint_name;
-  joint_name.push_back("joint1");
-  joint_name.push_back("joint2");
-  joint_name.push_back("joint3");
-  joint_name.push_back("joint4");
-  setJointSpacePath(joint_name, start_joint_stamp_, PATH_TIME);
+  setJointSpacePath(joint_name_, start_joint_stamp_, PATH_TIME);
   usleep(1000000);
-  setJointSpacePath(joint_name, end_joint_stamp_, PATH_TIME);
-  ROS_INFO("run\n");
+  setJointSpacePath(joint_name_, end_joint_stamp_, PATH_TIME);
+  ROS_INFO("Move by recorded Point\n");
 }
 
-//hand guide move path
-void OpenManipulatorTeleop::handGuideMovePath(const std_msgs::Bool::ConstPtr &msg)
+void OpenManipulatorTeleop::handGuideMovePathCallback(const std_msgs::Bool::ConstPtr &msg)
 {
   playstamp = msg->data;
   buffer_index_ = 0;
+  ROS_INFO("Move by recorded Path\n");
+}
+
+bool OpenManipulatorTeleop::setActuatorState(bool actuator_state)
+{
+  open_manipulator_msgs::SetActuatorState srv;
+  srv.request.set_actuator_state = actuator_state;
+
+  if(set_actuator_state_client_.call(srv))
+  {
+    return srv.response.is_planned;
+  }
+  return false;
 }
 
 std::vector<double> OpenManipulatorTeleop::getPresentJointAngle()
@@ -203,26 +189,6 @@ std::vector<double> OpenManipulatorTeleop::getPresentGripperAngle()
 std::vector<double> OpenManipulatorTeleop::getPresentKinematicsPose()
 {
   return present_kinematic_position_;
-}
-
-//inputkinematic position return
-std::vector<double> OpenManipulatorTeleop::getInputKinematicsPose()
-{
-  return input_kinematic_position_;
-}
-
-bool OpenManipulatorTeleop::setJointSpacePathFromPresent(std::vector<std::string> joint_name, std::vector<double> joint_angle, double path_time)
-{
-  open_manipulator_msgs::SetJointPosition srv;
-  srv.request.joint_position.joint_name = joint_name;
-  srv.request.joint_position.position = joint_angle;
-  srv.request.path_time = path_time;
-
-  if(goal_joint_space_path_from_present_client_.call(srv))
-  {
-    return srv.response.is_planned;
-  }
-  return false;
 }
 
 bool OpenManipulatorTeleop::setJointSpacePath(std::vector<std::string> joint_name, std::vector<double> joint_angle, double path_time)
@@ -252,23 +218,6 @@ bool OpenManipulatorTeleop::setToolControl(std::vector<double> joint_angle)
   return false;
 }
 
-bool OpenManipulatorTeleop::setTaskSpacePathFromPresentPositionOnly(std::vector<double> kinematics_pose, double path_time)
-{
-  open_manipulator_msgs::SetKinematicsPose srv;
-  srv.request.planning_group = priv_node_handle_.param<std::string>("end_effector_name", "gripper");
-  srv.request.kinematics_pose.pose.position.x = kinematics_pose.at(0);
-  srv.request.kinematics_pose.pose.position.y = kinematics_pose.at(1);
-  srv.request.kinematics_pose.pose.position.z = kinematics_pose.at(2);
-  srv.request.path_time = path_time;
-
-  if(goal_task_space_path_from_present_position_only_client_.call(srv))
-  {
-    return srv.response.is_planned;
-  }
-  return false;
-}
-
-//add task space path ===> position only
 bool OpenManipulatorTeleop::setTaskSpacePathPositionOnly(std::vector<double> kinematics_pose, double path_time)
 {
   open_manipulator_msgs::SetKinematicsPose srv;
@@ -285,7 +234,6 @@ bool OpenManipulatorTeleop::setTaskSpacePathPositionOnly(std::vector<double> kin
   return false;
 }
 
-//add task space path ===> position + orientation
 bool OpenManipulatorTeleop::setTaskSpacePath(std::vector<double> kinematics_pose, std::vector<double> kinematics_orientation, double path_time)
 {
   open_manipulator_msgs::SetKinematicsPose srv;
@@ -308,116 +256,22 @@ bool OpenManipulatorTeleop::setTaskSpacePath(std::vector<double> kinematics_pose
   return false;
 }
 
-void OpenManipulatorTeleop::printText()
+void OpenManipulatorTeleop::printBufferSize()
 {
-  printf("\n");
-  printf("---------------------------\n");
-
-  // printf("Present Joint Angle J1: %.3lf J2: %.3lf J3: %.3lf J4: %.3lf\n",
-  //        getPresentJointAngle().at(0),
-  //        getPresentJointAngle().at(1),
-  //        getPresentJointAngle().at(2),
-  //        getPresentJointAngle().at(3));
-  // printf("Present Kinematics Position X: %.3lf Y: %.3lf Z: %.3lf\n",
-  //        getPresentKinematicsPose().at(0),
-  //        getPresentKinematicsPose().at(1),
-  //        getPresentKinematicsPose().at(2));
-  // printf("---------------------------\n");
-
+  printf("\n---------------------------\n");
   printf("Start Recording Trajectory\n");
   printf("Buffer Size : %d\n", (int)(record_buffer_.size()));
-  // printf("recorded Joint Angle J1: %.3lf J2: %.3lf J3: %.3lf J4: %.3lf\n",
-  //       record_buffer_.at(buffer_index_).joint_angle.at(0),
-  //       record_buffer_.at(buffer_index_).joint_angle.at(1),
-  //       record_buffer_.at(buffer_index_).joint_angle.at(2),
-  //       record_buffer_.at(buffer_index_).joint_angle.at(3));
 }
 
-void OpenManipulatorTeleop::setGoal(char ch)
+void OpenManipulatorTeleop::setMode(char ch)
 {
   std::vector<double> goalPose;  goalPose.resize(3, 0.0);
   std::vector<double> goalJoint; goalJoint.resize(NUM_OF_JOINT, 0.0);
+  std::vector<double> goalOrientation;  goalOrientation.resize(4);
 
-  if(ch == 'g' || ch == 'G')
+  if(ch == '1')
   {
-    printf("input : g \topen gripper\n");
-    std::vector<double> joint_angle;
-
-    joint_angle.push_back(0.01);
-    setToolControl(joint_angle);
-  }
-  else if(ch == 'f' || ch == 'F')
-  {
-    printf("input : f \tclose gripper\n");
-    std::vector<double> joint_angle;
-    joint_angle.push_back(-0.01);
-    setToolControl(joint_angle);
-  }
-
-  else if(ch == '2')
-  {
-    printf("input : 2 \thome pose\n");
-    std::vector<std::string> joint_name;
-    std::vector<double> joint_angle;
-    double path_time = 2.0;
-
-    joint_name.push_back("joint1"); joint_angle.push_back(0.0);
-    joint_name.push_back("joint2"); joint_angle.push_back(-1.05);
-    joint_name.push_back("joint3"); joint_angle.push_back(0.35);
-    joint_name.push_back("joint4"); joint_angle.push_back(0.70);
-    setJointSpacePath(joint_name, joint_angle, path_time);
-  }
-  else if(ch == '1')
-  {
-    printf("input : 1 \tinit pose\n");
-
-    std::vector<std::string> joint_name;
-    std::vector<double> joint_angle;
-    double path_time = 2.0;
-    joint_name.push_back("joint1"); joint_angle.push_back(0.0);
-    joint_name.push_back("joint2"); joint_angle.push_back(0.0);
-    joint_name.push_back("joint3"); joint_angle.push_back(0.0);
-    joint_name.push_back("joint4"); joint_angle.push_back(0.0);
-    setJointSpacePath(joint_name, joint_angle, path_time);
-  }
-  else if(ch == '3')
-  {
-    printf("input : 3 \tprint orientation\n");
-
-    printf("input ori X: %.3lf Y: %.3lf Z: %.3lf\n",
-    input_kinematic_orientation_.at(0),
-    input_kinematic_orientation_.at(1),
-    input_kinematic_orientation_.at(2));
-
-    // std::vector<std::string> joint_name;
-    // std::vector<double> joint_angle;
-    // double path_time = 2.0;
-    // joint_name.push_back("joint1"); joint_angle.push_back(1.0);
-    // joint_name.push_back("joint2"); joint_angle.push_back(1.0);
-    // joint_name.push_back("joint3"); joint_angle.push_back(1.0);
-    // joint_name.push_back("joint4"); joint_angle.push_back(1.0);
-    // setJointSpacePath(joint_name, joint_angle, path_time);
-  }
-  else if(ch == '4')
-  {
-    printf("input : 4 \tnew pose by position modify\n");
-    //입력받아서하기
-    // input_kinematic_position_.at(0) = 0.1025;
-    // input_kinematic_position_.at(1) = 0.1060;
-    // input_kinematic_position_.at(2) = 0.2380;
-
-    goalPose.at(0) = input_kinematic_position_.at(0) - getPresentKinematicsPose().at(0);
-    goalPose.at(1) = input_kinematic_position_.at(1) - getPresentKinematicsPose().at(1);
-    goalPose.at(2) = input_kinematic_position_.at(2) - getPresentKinematicsPose().at(2);
-
-    setTaskSpacePathFromPresentPositionOnly(goalPose, PATH_TIME);
-  }
-  else if(ch == '5')
-  {
-    printf("input : 5 \tpose + orientation\n");
-
-    std::vector<double> goalOrientation;
-    goalOrientation.resize(4);
+    printf("MODE1 : \tby Position + Orientation\n");
 
     goalPose.at(0) = input_kinematic_position_.at(0);
     goalPose.at(1) = input_kinematic_position_.at(1);
@@ -430,69 +284,16 @@ void OpenManipulatorTeleop::setGoal(char ch)
 
     setTaskSpacePath(goalPose, goalOrientation, PATH_TIME);
   }
-  else if(ch == '6')
+  else if(ch == '2')
   {
-    printf("input : 6 \tpose + orientation\n");
-
-    // std::vector<double> goalOrientation;
-    // goalOrientation.resize(4);
+    printf("MODE2 : 6 \tby Position Only\n");
 
     goalPose.at(0) = input_kinematic_position_.at(0);
     goalPose.at(1) = input_kinematic_position_.at(1);
     goalPose.at(2) = input_kinematic_position_.at(2);
 
-    // goalOrientation.at(0) = input_kinematic_orientation_.at(0);
-    // goalOrientation.at(1) = input_kinematic_orientation_.at(1);
-    // goalOrientation.at(2) = input_kinematic_orientation_.at(2);
-    // goalOrientation.at(3) = input_kinematic_orientation_.at(3);
-
     setTaskSpacePathPositionOnly(goalPose, PATH_TIME);
   }
-  else if(ch == '7')
-  {
-    printf("input : 7 \tpose + orientation\n");
-    setActuatorState(false);
-  }
-  else if(ch == '8')
-  {
-    printf("input : 8 \tmove to joint angle\n");
-    
-    std::vector<std::string> joint_name;
-    std::vector<double> joint_angle;
-    double path_time = 2.0;
-    joint_name.push_back("joint1"); joint_angle.push_back(0.0);
-    joint_name.push_back("joint2"); joint_angle.push_back(0.0);
-    joint_name.push_back("joint3"); joint_angle.push_back(0.0);
-    joint_name.push_back("joint4"); joint_angle.push_back(0.0);
-    setJointSpacePath(joint_name, joint_angle, path_time);
-  }
-  else if(ch == '9')
-  {
-    printf("input : 9 \trecorded\n");
-    
-    std::vector<std::string> joint_name;
-    std::vector<double> joint_angle;
-    joint_name.push_back("joint1");
-    joint_name.push_back("joint2");
-    joint_name.push_back("joint3");
-    joint_name.push_back("joint4");
-    joint_angle = record_buffer_.at(buffer_index_).joint_angle;
-    std::vector<double> gripper_angle;
-    gripper_angle.push_back(record_buffer_.at(buffer_index_).tool_position);
-
-    if(record_buffer_.size() > buffer_index_)
-    {
-      setJointSpacePath(joint_name , joint_angle, LOOP_RATE * 10);
-      setToolControl(gripper_angle);
-      buffer_index_ ++;
-      printf("\n%d",(int)(buffer_index_));
-      printf("\nrecorded Joint Angle J1: %.3lf J2: %.3lf J3: %.3lf J4: %.3lf\n",
-        record_buffer_.at(buffer_index_).joint_angle.at(0),
-        record_buffer_.at(buffer_index_).joint_angle.at(1),
-        record_buffer_.at(buffer_index_).joint_angle.at(2),
-        record_buffer_.at(buffer_index_).joint_angle.at(3));
-    }
-  }  
 }
 
 void OpenManipulatorTeleop::publishCallback(const ros::TimerEvent&)
@@ -507,24 +308,19 @@ void OpenManipulatorTeleop::publishCallback(const ros::TimerEvent&)
     static int count = 0;
     if(!(count % 5))
     {
-      printText();
+      printBufferSize();
     }
     count ++;
   }
 
   if(playstamp && record_buffer_.size() > buffer_index_)
   {
-    std::vector<std::string> joint_name;
     std::vector<double> joint_angle;
-    joint_name.push_back("joint1");
-    joint_name.push_back("joint2");
-    joint_name.push_back("joint3");
-    joint_name.push_back("joint4");
     joint_angle = record_buffer_.at(buffer_index_).joint_angle;
     std::vector<double> gripper_angle;
     gripper_angle.push_back(record_buffer_.at(buffer_index_).tool_position);
 
-    setJointSpacePath(joint_name , joint_angle, LOOP_RATE * 10);
+    setJointSpacePath(joint_name_ , joint_angle, LOOP_RATE * 10);
     setToolControl(gripper_angle);
     printf("\n%d",(int)(buffer_index_));
     printf("\nrecorded Joint Angle J1: %.3lf J2: %.3lf J3: %.3lf J4: %.3lf\n",
